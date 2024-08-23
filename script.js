3d318ac03f39586b858c7776a9c552d5f21319f8
brocodile <dakzsh24@gmail.com>
2024-08-23 22:02:20 +0530

    Add 16*16 grid

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const container = document.querySelector("#container");
+for(var i = 0; i < 16; i++){
+    for(var j = 0; j < 16; j++){
+        const div = document.createElement('div');
+        div.setAttribute('class', 'colordiv');
+        container.appendChild(div);
+    }
+}
