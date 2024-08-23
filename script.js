d35f0022f3aedb7f83043c913b99475f7227702e
brocodile <dakzsh24@gmail.com>
2024-08-23 23:30:31 +0530

    Add Custom Sizing

--- script.js
+++ script.js
@@ -1,15 +1,65 @@
 const container = document.querySelector("#container");
-for(var i = 0; i < 16; i++){
-    for(var j = 0; j < 16; j++){
-        const div = document.createElement('div');
-        div.setAttribute('class', 'colordiv');
-        container.appendChild(div);
+const buttonContainer = document.createElement('div');
+const divContainer = document.createElement('div');
+
+buttonContainer.setAttribute("id", "controllers");
+divContainer.setAttribute("id", "color-container");
+
+container.appendChild(buttonContainer);
+container.appendChild(divContainer);
+
+const resButton = document.createElement('button');
+resButton.textContent = "Grid Size";
+buttonContainer.appendChild(resButton);
+
+resButton.addEventListener("click", () => {
+    var gsize = parseInt(prompt("Enter New Grid Size (less than 100)"));
+    while(gsize > 100){
+        gsize = parseInt(prompt("Enter New Grid Size (less than 100)"));
     }
+    divDeleter();
+    divCreator(gsize);
+    
+});
+function divDeleter(){
+    divArray.forEach((divs) => {
+        divs.remove();
+      });
 }
 
-const divArray = document.querySelectorAll(".colordiv");
-divArray.forEach((divs) => {
-    divs.addEventListener("mousemove", () => {
-        divs.style["background-color"] = "yellow";
-    })
-  });
+function blockSize(size){
+    var width = 512/size;
+    divArray.forEach((divs) => {
+        divs.style["width"] = String(width) + 'px';
+        divs.style["height"] = String(width) + 'px';
+        })
+}
+
+function divCreator(size){
+    for(var i = 0; i < size; i++){
+        for(var j = 0; j < size; j++){
+            const div = document.createElement('div');
+            div.setAttribute('class', 'colordiv');
+            divContainer.appendChild(div);
+        }
+    }
+    divArrayMaker();
+    blockSize(size);
+    eventmaker();
+}
+
+let divArray = document.querySelectorAll(".colordiv");
+function eventmaker(){
+    divArray.forEach((divs) => {
+        divs.addEventListener("mousemove", () => {
+            divs.style["background-color"] = "yellow";
+        })
+    });
+}
+function divArrayMaker(){
+    divArray = document.querySelectorAll(".colordiv");
+}
+
+
+divCreator(16);
+eventmaker();
